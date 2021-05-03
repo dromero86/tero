de9b940e85ba6e848f42e8f77e22491cea6bf453
Daniel Romero <dromero@ymail.com>
2021-05-03 10:09:01 -0300

    Fixed many errors

--- app/vendor/Email.php
+++ app/vendor/Email.php
@@ -26,6 +26,30 @@
  * @author		ExpressionEngine Dev Team
  * @link		http://codeigniter.com/user_guide/libraries/email.html
  */
+
+define('FILE_READ_MODE', 0644);
+define('FILE_WRITE_MODE', 0666);
+define('DIR_READ_MODE', 0755);
+define('DIR_WRITE_MODE', 0777);
+
+/*
+|--------------------------------------------------------------------------
+| File Stream Modes
+|--------------------------------------------------------------------------
+|
+| These modes are used when working with fopen()/popen()
+|
+*/
+
+define('FOPEN_READ',							'rb');
+define('FOPEN_READ_WRITE',						'r+b');
+define('FOPEN_WRITE_CREATE_DESTRUCTIVE',		'wb'); // truncates existing file data, use with care
+define('FOPEN_READ_WRITE_CREATE_DESTRUCTIVE',	'w+b'); // truncates existing file data, use with care
+define('FOPEN_WRITE_CREATE',					'ab');
+define('FOPEN_READ_WRITE_CREATE',				'a+b');
+define('FOPEN_WRITE_CREATE_STRICT',				'xb');
+define('FOPEN_READ_WRITE_CREATE_STRICT',		'x+b');
+
 class Email {
 
 	var $config_file 	= "app/config/email.json";
@@ -1986,7 +2010,7 @@ class Email {
 	 */
 	protected function _set_error_message($msg, $val = '')
 	{
-		echo str_replace('%s', $msg,$val )."<br />";
+		/*echo str_replace('%s', $msg,$val )."<br />";*/
 		/*
 		$CI =& get_instance();
 		$CI->lang->load('email');
